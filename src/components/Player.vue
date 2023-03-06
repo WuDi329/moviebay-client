3c564a170b6d9597d8352d33aab722522390df50
wudi <wuuudiii@qq.com>
2023-03-06 14:08:48 +0800

    first commit

---
+++ src/components/Player.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- 这里访问stream目录下的第一个分段 -->
+  <video id="player" src="http://localhost:3000/stream/1" controls autoplay></video>
+</template>
+
+<script>
+export default {
+  name: "Player",
+  async created() {
+    console.log(this.$route.params.id);
+  }
+};
+</script>
